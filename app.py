dc65e40d0a8e0b333c74a162a34ba9f6c4a08f15
Ruari Hodgin <ruarihodgin99@outlook.com>
2022-10-28 16:45:53 +0100

    added flask-login, user dashboard

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from sqlalchemy import Table, create_engine
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 import os
 import pandas as pd
 from datetime import datetime
@@ -17,44 +16,34 @@ from datetime import datetime
 #Create flask instance
 app = Flask(__name__)
 #Add database
-engine = create_engine('mysql+pymysql://root:jqtnnhj2@localhost/users')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:jqtnnhj2@localhost/users'
+engine = create_engine('mysql+pymysql://root:jqtnnhj2@localhost/userinfo')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:jqtnnhj2@localhost/userinfo'
 app.config['SECRET_KEY'] = os.urandom(12)
 #Initialise database
 with app.app_context():
     db = SQLAlchemy(app)
+#Flask_login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Login.query.get(int(user_id))
+
 
 #Create user model
-class Users(db.Model):
+class Login(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String(50), nullable=False)
-    sname = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    org = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
-    uname = db.Column(db.String(50), nullable=False, unique=True)
-    pwd = db.Column(db.String(50), nullable=False)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow())
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
 
-    #Create a string
     def __repr__(self):
-        return '<Username %r>' % self.id
-
-#Create form class
-class registerForm(FlaskForm):
-    fname = StringField("Enter first name", validators=[DataRequired()])
-    sname = StringField("Enter surname", validators=[DataRequired()])
-    uname = StringField("Enter username", validators=[DataRequired()])
-    pwd = StringField("Enter password", validators=[DataRequired()])
-    repwd = StringField("Re-enter password", validators=[DataRequired()])
-    email = StringField("Enter email address", validators=[DataRequired()])
-    org = StringField("Select your organisation", validators=[DataRequired()])
-    role = StringField("Select your role", validators=[DataRequired()])
-    submit = SubmitField("Register")
+        return '<User ID %r>' % self.id
 
 class loginForm(FlaskForm):
     uname = StringField("Enter username", validators=[DataRequired()])
-    pwd = StringField("Enter password", validators=[DataRequired()])
+    pwd = PasswordField("Enter password", validators=[DataRequired()])
     submit = SubmitField("Login")
 
 #Create routes (pages)
@@ -71,63 +60,36 @@ def login():
     form = loginForm()
     #validate form
     if form.validate_on_submit():
-        uname=form.uname.data
-        pwd=form.pwd.data
-        form.uname.data=''
-        form.pwd.data=''
-        flash("Login successful")
+        user = Login.query.filter_by(username=form.uname.data).first()
+        if user is None:
+            flash("Username incorrect, please try again.")
+        else:
+            if check_password_hash(user.password,form.pwd.data):
+                flash("Login Successful!")
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Incorrect password, please try again.")
     return render_template('login.html',
                             uname = uname,
                             pwd = pwd,
                             form = form
                            )
 
-#register page
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    fname = None
-    sname = None
-    uname = None
-    pwd = None
-    repwd = None
-    email = None
-    org = None
-    role = None
-    form = registerForm()
-    if form.validate_on_submit():
-        user = Users.query.filter_by(uname = form.uname.data).first
-        if user is None:
-            uemail = Users.query.filter_by(email = form.email.data).first
-            if uemail is None:
-                if pwd==repwd:
-                    user = Users(fname=form.fname.data, sname=form.sname.data,
-                                 uname=form.uname.data, pwd=form.pwd.data,
-                                 email=form.email.data, org=form.org.data,
-                                 role=form.role.data)
-                    with app.app_context():
-                        db.session.add(user)
-                        db.session.commit()
-                    flash("Account created successfully")
-                else:
-                    flash("Passwords do not match!")
-            else:
-                flash("Email already registered!")
-        else:
-            flash("Username already registered!")
-
-    return render_template('register.html',
-                           fname=fname,
-                           sname=sname,
-                           uname=uname,
-                           pwd=pwd,
-                           repwd=repwd,
-                           email=email,
-                           org=org,
-                           role=role,
-                           form=form
-                           )
+#Logout functionality
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('home'))
 
 #data page
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/data/<fname>+<sname>')
 def data(fname, sname):
     return render_template('data.html', fname=fname, sname=sname)
